f1ce7cf98645662b60df75d5b1b96aa6af066ca0
Joakim Haugen <joakim.haugen@gmail.com>
2020-06-20 17:01:11 +0200

    Fix syntax error conditional requirements

--- conanfile.py
+++ conanfile.py
@@ -54,7 +54,7 @@ class QwtConan(ConanFile):
 
     def requirements(self):
 
-        if self.compiler.version == "6" and self.compiler == "gcc":
+        if self.settings.compiler.version == "6" and self.settings.compiler == "gcc":
             self.requires("qt/5.12.8@bincrafters/stable")
         else:
             self.requires("qt/5.15.0@bincrafters/stable")
